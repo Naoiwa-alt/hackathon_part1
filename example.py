1723555a3ca9f822a3e6abfccd5973b68a8804c5
Naoiwa <beer.wavely@gmail.com>
2025-07-23 23:01:26 +0700

    revise

--- example.py
+++ example.py
@@ -1,9 +1,8 @@
-# 6. example.pyをリポジトリにコミットし、pushしてください。
 # 1. example.pyを作成して「hello world」と出力してください。
-print("hellow world")
+print("hello world")
 # 2. greet関数を実装し、「こんにちは」と出力するようにしてください。関数を呼び出して、実際に出力されることを確認してください。
-def greet ():
-    print("こんにちは")
+def greet():
+    return "こんにちは"
 greet()
 # 3. nameを引数に取り、「私の名前は{name}です」と出力するprint_name関数を実装し、関数を呼び出して動作を確認してください。
 name = input("enter your name here")
@@ -12,12 +11,13 @@ def print_name(name):
 print_name(name)
 # 4. 「おはようございます」という文字列を戻り値として返すget_greet関数を実装し、戻り値をprint関数で出力してください。
 def get_greet():
-    return print("「おはようございます」")
-get_greet()
+    return "「おはようございます」"
+print(get_greet())
 # 5. a, bを引数に取り、その足し算の結果を戻り値として返すadd関数を実装し、関数を呼び出して結果をprint関数で出力してください。
+def add(a,b):
+    return a + b
 a = int(input("a="))
 b = int(input("b="))
-def add(a,b):
-    result = a + b
-    return print(result)
-add(a,b)
+print(add(a,b))
+
+# 6. example.pyをリポジトリにコミットし、pushしてください。
